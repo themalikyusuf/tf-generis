946173698ee4bc1bc2c1c4b2c6c81a5250cec503
Abdulmalik Yusuf <themalikyusuf@gmail.com>
2020-04-16 17:19:53 +0100

    staging test

--- main.tf
+++ main.tf
@@ -3,7 +3,19 @@ provider "aws" {
   region     = "us-east-1"
 }
 
-resource "aws_instance" "example" {
-  ami           = "ami-2757f631"
+resource "aws_instance" "staging" {
+  ami           = "ami-"
   instance_type = "t2.micro"
+  key_name      = "keys"
+
+  provisioner "local-exec" {
+      command = "echo ${aws_instance.staging.public_ip} > ip_address.txt"
+    }
 }
+
+resource "aws_eip" "ip" {
+    vpc = true
+    instance = aws_instance.staging.id
+}
+
+

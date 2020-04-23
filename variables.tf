b3620e9f9598a0f411da79eb5e1f2be3108d1abd
Abdulmalik Yusuf <themalikyusuf@gmail.com>
2020-04-23 18:42:16 +0100

    add vars and output

--- variables.tf
+++ variables.tf
@@ -1,11 +1,55 @@
-variable "profile" { default = "default" }
+variable "profile" { 
+	default = "default"
+}
 
-variable "region" { default = "us-east-1" }
+variable "region" {
+	default = "us-east-1"
+}
 
-variable "amis" {}
+variable "vpc_name" {
+  description = "Name of VPC"
+  type        = string
+  default     = "staging-vpc"
+}
 
-variable "instance_type" {}
+variable "vpc_cidr" {
+  description = "CIDR block for VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "vpc_azs" {
+  description = "Availability zones for VPC"
+  type        = list
+  default     = ["us-east-1a", "us-east-1b", "us-east-1c"]
+}
+
+variable "vpc_private_subnets" {
+  description = "Private subnets for VPC"
+  type        = list(string)
+  default     = ["10.0.1.0/24", "10.0.2.0/24"]
+}
+
+variable "vpc_public_subnets" {
+  description = "Public subnets for VPC"
+  type        = list(string)
+  default     = ["10.0.101.0/24", "10.0.102.0/24"]
+}
+
+variable "vpc_tags" {
+  description = "Tags to apply to resources created by VPC module"
+  type        = map(string)
+  default     = {
+    Environment = "staging"
+  }
+}
 
 variable "key_name" {}
 
-variable "private_key" {}
+variable "amis" {}
+
+variable "instance_type" {
+	description = "EC2 instance type"
+	type 				= string
+	default			= "t2.micro"
+}

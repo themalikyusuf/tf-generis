aa351eac1af8a4822d71d99e64e448792309920b
Abdulmalik Yusuf <themalikyusuf@gmail.com>
2020-04-23 18:42:03 +0100

    learning modules

--- main.tf
+++ main.tf
@@ -1,29 +1,39 @@
 provider "aws" {
-  profile    = var.profile
-  region     = var.region
+	profile = var.profile
+  region  = var.region
 }
 
-resource "aws_instance" "staging" {
-  ami           = var.amis[var.region]
-  instance_type = var.instance_type
-  key_name      = var.key_name
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "2.21.0"
 
-  connection {
-    type     = "ssh"
-    user     = "ubuntu"
-    private_key = file(var.private_key)
-    host     = self.public_ip
-  }
+  name = var.vpc_name
+  cidr = var.vpc_cidr
 
-  provisioner "remote-exec" {
-    inline = [
-      "sudo apt-get update",
-      "sudo apt-get install apache2 -y"
-    ]
-  }
+  azs             = var.vpc_azs
+  private_subnets = var.vpc_private_subnets
+  public_subnets  = var.vpc_public_subnets
+
+  enable_nat_gateway = true
+
+  tags = var.vpc_tags
 }
 
-resource "aws_eip" "ip" {
-  vpc = true
-  instance = aws_instance.staging.id
+module "ec2_instances" {
+  source  = "terraform-aws-modules/ec2-instance/aws"
+  version = "2.12.0"
+
+  name           = "staging-cluster"
+  instance_count = 1
+
+  ami                    = var.amis[var.region]
+  instance_type          = var.instance_type
+  key_name     					 = var.key_name
+  vpc_security_group_ids = [module.vpc.default_security_group_id]
+  subnet_id              = module.vpc.public_subnets[0]
+
+  tags = var.vpc_tags
 }
+
+
+

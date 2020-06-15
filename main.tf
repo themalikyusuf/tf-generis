93c675790644c7789151baf198228d788f4918f5
Abdulmalik Yusuf <themalikyusuf@gmail.com>
2020-06-15 15:22:59 +0100

    feat: create vpc and aurora

--- main.tf
+++ main.tf
@@ -1,39 +1,65 @@
-provider "aws" {
-	profile = var.profile
-  region  = var.region
+locals {
+  name = "aws-environment"
+  common_tags = {
+    Name = "aws"
+    Environment = "staging"
+  }
+}
+
+data "aws_availability_zones" "available" {
+  state = "available"
 }
 
 module "vpc" {
-  source  = "terraform-aws-modules/vpc/aws"
-  version = "2.21.0"
+  source = "terraform-aws-modules/vpc/aws"
 
-  name = var.vpc_name
+  name = local.name
   cidr = var.vpc_cidr
 
-  azs             = var.vpc_azs
-  private_subnets = var.vpc_private_subnets
-  public_subnets  = var.vpc_public_subnets
+  azs             = [data.aws_availability_zones.available.names[0], data.aws_availability_zones.available.names[1], data.aws_availability_zones.available.names[2]]
+  private_subnets = [cidrsubnet(var.vpc_cidr, 4, 0), cidrsubnet(var.vpc_cidr, 4, 1), cidrsubnet(var.vpc_cidr, 4, 2)]
+  public_subnets  = [cidrsubnet(var.vpc_cidr, 4, 3), cidrsubnet(var.vpc_cidr, 4, 4), cidrsubnet(var.vpc_cidr, 4, 5)]
 
   enable_nat_gateway = true
 
-  tags = var.vpc_tags
+  tags = local.common_tags
 }
 
-module "ec2_instances" {
-  source  = "terraform-aws-modules/ec2-instance/aws"
-  version = "2.12.0"
+module "db" {
+  source  = "terraform-aws-modules/rds-aurora/aws"
+  version = "~> 2.0"
 
-  name           = "staging-cluster"
-  instance_count = 1
+  name                            = local.name
 
-  ami                    = var.amis[var.region]
-  instance_type          = var.instance_type
-  key_name     					 = var.key_name
-  vpc_security_group_ids = [module.vpc.default_security_group_id]
-  subnet_id              = module.vpc.public_subnets[0]
+  engine                          = "aurora-mysql"
+  engine_version                  = "5.7.12"
 
-  tags = var.vpc_tags
+  vpc_id                          = module.vpc.vpc_id
+  subnets                         = module.vpc.private_subnets
+
+  replica_count                   = 1
+  allowed_cidr_blocks             = [var.vpc_cidr]
+  instance_type                   = "db.t2.medium"
+  storage_encrypted               = true
+  apply_immediately               = true
+  monitoring_interval             = 10
+
+  db_parameter_group_name         = aws_db_parameter_group.db_param_group.name
+  db_cluster_parameter_group_name = aws_rds_cluster_parameter_group.db_cluster_param_group.name
+
+  enabled_cloudwatch_logs_exports = ["audit", "error", "general", "slowquery"]
+
+  tags = local.common_tags
 }
 
+resource "aws_db_parameter_group" "db_param_group" {
+  name   = local.name
+  family = "aurora-mysql5.7"
+}
 
+resource "aws_rds_cluster_parameter_group" "db_cluster_param_group" {
+  name        = local.name
+  family      = "aurora-mysql5.7"
+  description = "RDS default cluster parameter group"
+}
 
